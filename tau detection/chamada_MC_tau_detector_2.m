66a7f2c8828bfbdafa678d9b5f0e923f478043c1
mbmartins <mbrittom@gmail.com>
2019-04-16 16:00:49 -0300

    Usar PATV no sinal analítico (mag ou fase) para reduzir o ruído antes de fazer o gradiente!

--- tau detection/chamada_MC_tau_detector_2.m
+++ tau detection/chamada_MC_tau_detector_2.m
@@ -6,18 +6,20 @@ SNR = 35; %SNR em db
 Pin = 90; %fase inicial em graus
 
 %conjuntos de limiares para a simulação
-lim_mag = 0.1:0.1:7; %limiar do detector de magnitude (x mediana)
-lim_fase = 1:0.2:7;%limiar do detector de fase (x mediana)
+%lim_mag = 0.1:0.1:7; %limiar do detector de magnitude (x mediana)
+%lim_fase = 1:0.2:7;%limiar do detector de fase (x mediana)
 %obs: nos valores abaixo, praticamente atua somente o detector de mag
-%lim_mag = 1; %limiar do detector de magnitude (x mediana)
-%lim_fase = 7;%limiar do detector de fase (x mediana)
+lim_mag = 1; %limiar do detector de magnitude (x mediana)
+lim_fase = 7;%limiar do detector de fase (x mediana)
 
-Nruns = 10000; %numero de rodadas de Monte Carlo
-KxS = 0.2;
+%Nruns = 10000; %numero de rodadas de Monte Carlo
+Nruns = 1;
+KxS = 0.1;
 
 for m = 1:length(lim_mag)
     for n = 1:length(lim_fase)
-        tau_pp_lims = [0.4990 0.5010]; %conjunto de posições relativas de tau dentro da janela
+        %tau_pp_lims = [0.4990 0.5010]; %conjunto de posições relativas de tau dentro da janela
+        tau_pp_lims = [0.2 0.2];
         [tau_errors,tau_pp, extremos(m,n), det_mag(m,n),det_fase(m,n),det_nan(m,n)] = MC_tau_error_hibrid_detector_v4_taurand(KxS,SNR,Pin,lim_mag(m),lim_fase(n),tau_pp_lims,Nruns);
         display([lim_mag(m) lim_fase(n)])
         
