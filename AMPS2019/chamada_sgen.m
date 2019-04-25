f361515955d555364ca0ee3c69bbe61add9b789e
mbmartins <mbrittom@gmail.com>
2019-04-25 16:09:16 -0300

    modif de hoje

--- AMPS2019/chamada_sgen.m
+++ AMPS2019/chamada_sgen.m
@@ -1,20 +1,18 @@
 clear all; close all; clc;
 
 %nominal parameters
-F0 = 60; F1 = 60;
-Fs = 4800;
-Ps = 90;
-NCycles = 6;
+F0 = 60; F1 = 59.8; Fs = 4800;
+Ps = 90; NCycles = 6;
 NSamples = floor(NCycles*Fs/F0);
 n = 1:NSamples;
 
 %parameters to set
-SNR = 90;
-tau1 = 0.5;  % in [%] of the time window
+SNR = 50;
+tau1 = 0.0;  % in [%] of the time window
 tau2 = tau1+(Fs/F0)/NSamples;  %+1 cycle
-KaS = 10; %[degrees]
-KxS = -0.10; % [relative step]
-th_gmi = 1e-3; th_fi = 1e-3;
+KaS = -10; %[degrees]
+KxS = -0.1; % [relative step]
+th_gmi = 1e-4; th_fi = 1e-4;
 th_gmi_v(1:NSamples) = th_gmi; %threshold for magnitude detection
 th_fi_v(1:NSamples) = th_fi; %threshold for frequency detection
 
@@ -78,17 +76,23 @@ plot(n,detector_gmi,'b');  ylabel('Detection signal')
 %%%%%%%%%%%  estimate taus
 
 %detector_gmi
-[gmi_max,imax_gmi(1)] = maxk(detector_gmi,1);
+[gmi_max(1),imax_gmi(1)] = maxk(detector_gmi,1);
 %[gmi_min,imin] = min(detector_gmi);
 % ignores adjacent samples
-br = floor(0.02*NSamples);
-detector_gmi(imax_gmi-br:imax_gmi+br) = 0;
+%br = floor(0.02*NSamples);
+aind = imax_gmi-1;
+while detector_gmi(aind)>th_gmi
+    detector_gmi(aind:imax_gmi) = 0;
+    aind = aind - 1;
+end
+aind = imax_gmi+1;
+while detector_gmi(aind)>th_gmi
+    detector_gmi(imax_gmi:aind) = 0;
+    aind = aind + 1;
+end
 hold on; plot(n,detector_gmi,'red',n,th_gmi_v,'k--')
 legend('d1_{gmi}','d2_{gmi}', 'Location','southeast')
-[gmi_max2,imax_gmi(2)] = maxk(detector_gmi,1);
-tau_est_gmi = sort(imax_gmi); %error in samples
-tau1_error_gmi = (tau_est_gmi(1) - tau1*NSamples) %error in [dt]
-tau2_error_gmi = (tau_est_gmi(2) - tau2*NSamples) %error in [dt]
+[gmi_max(2),imax_gmi(2)] = maxk(detector_gmi,1);
 
 %detector_fi
 detector_fi = abs(gradient(x_fi));
@@ -100,22 +104,57 @@ legend('Data','Estimated signal', 'Location','southeast')
 subplot(2,1,2)
 plot(n,detector_fi,'b');  ylabel('Detection signal')
 
-[fi_max,imax_fi(1)] = maxk(detector_fi,1); %detects first tau
-detector_fi(imax_fi-br:imax_fi+br) = 0; %ignores adjacent samples
+[fi_max(1),imax_fi(1)] = maxk(detector_fi,1); %detects first peak
+
+%ignore adjacent samples
+%detector_fi(imax_fi-br:imax_fi+br) = 0; 
+aind = imax_fi-1;
+while detector_fi(aind)>th_fi
+    detector_fi(aind:imax_fi) = 0;
+    aind = aind - 1;
+end
+aind = imax_fi+1;
+while detector_fi(aind)>th_fi
+    detector_fi(imax_fi:aind) = 0;
+    aind = aind + 1;
+end
 hold on; plot(n,detector_fi,'red',n,th_fi_v,'k--')
 legend('d1_{fi}','d2_{fi}', 'Location','southeast')
-[fi_max2,imax_fi(2)] = maxk(detector_fi,1);
-tau_est_fi = sort(imax_fi); %error in samples
-tau1_error_fi = (tau_est_fi(1) - tau1*NSamples) %error in [dt]
-tau2_error_fi = (tau_est_fi(2) - tau2*NSamples) %error in [dt]
+[fi_max(2),imax_fi(2)] = maxk(detector_fi,1); %detects second peak
 
-% 1 - check if estimates are valid
-if (tau_est_fi(1) <= th_fi)
-    tau_est_fi = NaN;
+% error calculation
+[tau_est_gmi, gmi_ind] = sort(imax_gmi); %error in samples
+tau_est_gmi(gmi_max < th_gmi) = NaN; % check if estimates are valid
+tau_error_gmi = (tau_est_gmi - [tau1 tau2]*NSamples) %error in [dt]
 
-% individualmente ou em grupo??
-crit_fi = fi_max/th_fi;
-crit_gmi = gmi_max/th_gmi;
+[tau_est_fi,fi_ind] = sort(imax_fi); %tau estimated in [samples]
+tau_est_fi(fi_max < th_fi) = NaN; % check if estimates are valid
+tau_error_fi = (tau_est_fi - [tau1 tau2]*NSamples) %error in [dt]
+
+%criterio de escolha mag x freq com denoising: usar a maior diferença para 
+% o limiar
+crit = gmi_max(gmi_ind) - th_gmi > fi_max(fi_ind) - th_fi;
+tau_est(crit) = tau_est_gmi(crit)
+tau_est(~crit) = tau_est_fi(~crit)
+tau_error = tau_est - [tau1 tau2]*NSamples
 
 %calcular frequencia pela inclinação da fase
+P = polyfit(n,p_fi',1);
+f_est = P(1)*Fs/(2*pi);
+FE = (f_est - F1)*100/F1
 
+
+%problemas: 
+% 1 - indices excedendo as bordas (na hora de excluir do sinal de
+% detecção) -> implementar um teste para isso não ocorrer
+% por exemplo, baseado no th_gmi - ok
+% 2 - picos ocorrendo perto das bordas (falsos ou verdadeiros) quando f1
+% está fora da nominal
+% -> implementar exclusão próximo das bordas (2 a 5%) ??
+% -> picos verdadeiros terão que ser excluídos -> limitação do método
+% talvez identificar de alguma forma o número de degraus pelo PATV
+% o PATV "filtra" as não idealidades de Hilbert próximo a borda
+% 3 - redução do desempenho da detecção quando os degraus estão próximos da
+% borda
+% 4 - quando só existe tau2, fazendo tau1<=0 -> ver como o sort lida com
+% NaN
