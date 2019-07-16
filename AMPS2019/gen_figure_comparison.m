6cb485b24f6e52ddba3daa7a5c076d23dc23c046
mbmartins <mbrittom@gmail.com>
2019-07-16 15:50:20 -0300

    PATV insertions

--- AMPS2019/gen_figure_comparison.m
+++ AMPS2019/gen_figure_comparison.m
@@ -1,62 +1,85 @@
 %generation of performance comparison
 clear all; close all; clc
 
-%SNR = 30;
+SNR = 60;
 %SNR = 60:-10:30;
-SNR = 60:-10:30;
-Pss = 0:15:90;
+%SNR = 90:-10:30;
 
-%angulo fixo
-%Pss = 90;
+%angulo phi_0, fixo ou tabela
+Pss = 90;
+%Pss = 0:15:90;
 
 %fixed parameters
 f0 = 60;
 tau1 = 0.5;  % in [%] of the time window
 SAG_cycles = 10; %duration of SAG 
-KaS = 10; %[degrees]
-KxS = -0.0; % [relative step]
+KaS = 0.0; %[degrees]
+KxS = -0.1; % [relative step]
 
-km = 10;
-kf = 3; % a partir de 8 praticamente fase não atua...
+km = 3;
+kf = 10; % a partir de 8 praticamente fase não atua...
 
-HE_eps =  zeros(1,length(SNR));
-NLHE_eps =  zeros(1,length(SNR));
-MCruns = 10000;
+MCruns = 1000;
 
-for ps = 1:length(Pss)
-    for s = 1:length(SNR)
-        eps_NLHE = 0;
-        eps_HE = 0;
-        %MC for HE
+for ps = 1:length(Pss) % loop for different initial phases
+    for s = 1:length(SNR)  % loop for different SNRs
+        %MC engine
         for r = 1:MCruns  %taking the eps errors for tau1 only
-            Ps = Pss(ps);
-            %Ps_rand(r) = 90*rand(1,1);
-            %Ps = Ps_rand(r);
-            [tau_error_HE(r,:),FE(r,:)] = HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles,km,kf);
-    %        [FE_NL(r), tau_error_NLHE(r,:)] = NLHE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles);
-        crit_tau(r) = abs(tau_error_HE(r,1))>2;
-        crit_nan(r) = isnan(tau_error_HE(r,1));
-        crit_HE(r) = (crit_tau(r) || crit_nan(r));
+            % Fixed phi_0
+            %Ps = Pss(ps);
+            
+            % random phi_0 from uniform distribution
+            Ps_rand(r) = 90*rand(1,1);
+            Ps = Ps_rand(r);
+            
+            [tau_error_HE(r,:),FE_HE(r,:)] = HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles,km,kf);
+            [tau_error_PATV_HE(r,:),FE_PATV_HE(r,:),dmax(r,:)] = PATV_HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles);
+        
+        %Performance of tau estimation    
+        crit_tau_HE(r) = abs(tau_error_HE(r,1))>2;
+        crit_nan_HE(r) = isnan(tau_error_HE(r,1));
+        crit_HE(r) = (crit_tau_HE(r) || crit_nan_HE(r));
+        
+        crit_tau_PATV_HE(r) = abs(tau_error_PATV_HE(r,1))>2;
+        crit_nan_PATV_HE(r) = isnan(tau_error_PATV_HE(r,1));
+        crit_PATV_HE(r) = (crit_tau_PATV_HE(r) || crit_nan_PATV_HE(r));
+        
     %        eps_NLHE = eps_NLHE + (tau_error_NLHE(r,1)>2 || isnan(tau_error_NLHE(r,1)));
         end
-        false_pos(ps,s) = sum(crit_tau)*100/MCruns;
-        false_neg(ps,s) = sum(crit_nan)*100/MCruns;
-        eps_HE = sum(crit_HE);
-        FE_std(ps,s) = std(FE)/f0;
-        FE_mean(ps,s) = mean(FE)/f0;
-        FE_box(s,:) = FE/f0; %[Hz/Hz]
-        %NLHE_eps(s) = eps_NLHE*100/MCruns; % in [%]
-        HE_eps(ps,s) = eps_HE*100/MCruns; % in [%]
+        false_pos_HE(ps,s) = sum(crit_tau_HE)*100/MCruns;
+        false_neg_HE(ps,s) = sum(crit_nan_HE)*100/MCruns;
+        false_pos_PATV_HE(ps,s) = sum(crit_tau_PATV_HE)*100/MCruns;
+        false_neg_PATV_HE(ps,s) = sum(crit_nan_PATV_HE)*100/MCruns;
+        
+        eps_HE(ps,s) = sum(crit_HE)*100/MCruns; % in [%];
+        eps_PATV_HE(ps,s) = sum(crit_PATV_HE)*100/MCruns; % in [%];
+        
+        FE_std_HE(ps,s) = std(FE_HE)/f0;
+        FE_mean_HE(ps,s) = mean(FE_HE)/f0;
+        FE_box(s,:) = FE_HE/f0; %[Hz/Hz]
+
+        FE_std_PATV_HE(ps,s) = std(FE_PATV_HE)/f0;
+        FE_mean_PATV_HE(ps,s) = mean(FE_PATV_HE)/f0;
+        FE_box(s,:) = FE_PATV_HE/f0; %[Hz/Hz]
+        
     end
 end
 
-HE_eps
+
+%FE_std_HE
+%FE_std_PATV_HE
+eps_HE
+eps_PATV_HE
+beep
+FE_corr = FE_mean_PATV_HE - 0.002213
+dmax_mean = mean(dmax)
+dmax_std = std(dmax)
 %createfigure1(SNR, HE_eps')
 %createfigure2(SNR, HE_eps')
 
-boxplot(FE_box',SNR)
-xlabel('SNR [dB]')
-ylabel('FE [Hz/Hz]')
+% boxplot(FE_box',SNR)
+% xlabel('SNR [dB]')
+% ylabel('FE [Hz/Hz]')
 % 
 % figure
 % plot(SNR,FE_mean, SNR,FE_mean + FE_std, SNR, FE_mean - FE_std)
