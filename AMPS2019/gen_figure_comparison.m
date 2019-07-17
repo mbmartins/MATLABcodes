361f36c3e07b64399a9cc98cec098a8854e9dbe6
mbmartins <mbrittom@gmail.com>
2019-07-17 15:38:15 -0300

    incluindo nova simulação com os detectores hibridos

--- AMPS2019/gen_figure_comparison.m
+++ AMPS2019/gen_figure_comparison.m
@@ -1,66 +1,66 @@
 %generation of performance comparison
 clear all; close all; clc
 
-SNR = 60;
-%SNR = 60:-10:30;
+%SNR = 30;
+SNR = 60:-10:30;
 %SNR = 90:-10:30;
 
 %angulo phi_0, fixo ou tabela
 Pss = 90;
-%Pss = 0:15:90;
+Pss = 0:15:90;
 
 %fixed parameters
 f0 = 60;
 tau1 = 0.5;  % in [%] of the time window
 SAG_cycles = 10; %duration of SAG 
-KaS = 0.0; %[degrees]
-KxS = -0.1; % [relative step]
+KaS = 10.0; %[degrees]
+KxS = -0.0; % [relative step]
 
 km = 3;
-kf = 10; % a partir de 8 praticamente fase não atua...
+kf = 4; % a partir de 8 praticamente fase não atua...
 
-MCruns = 1000;
+MCruns = 10000;
 
 for ps = 1:length(Pss) % loop for different initial phases
     for s = 1:length(SNR)  % loop for different SNRs
         %MC engine
         for r = 1:MCruns  %taking the eps errors for tau1 only
             % Fixed phi_0
-            %Ps = Pss(ps);
+            Ps = Pss(ps);
             
             % random phi_0 from uniform distribution
-            Ps_rand(r) = 90*rand(1,1);
-            Ps = Ps_rand(r);
+%             Ps_rand(r) = 90*rand(1,1);
+%             Ps = Ps_rand(r);
             
             [tau_error_HE(r,:),FE_HE(r,:)] = HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles,km,kf);
-            [tau_error_PATV_HE(r,:),FE_PATV_HE(r,:),dmax(r,:)] = PATV_HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles);
-        
+            %[tau_error_PATV_HE(r,:),FE_PATV_HE(r,:),dmax(r,:)] = PATV_HE_estimator(SNR(s),KxS,KaS,Ps,tau1,SAG_cycles);
+            
         %Performance of tau estimation    
         crit_tau_HE(r) = abs(tau_error_HE(r,1))>2;
         crit_nan_HE(r) = isnan(tau_error_HE(r,1));
         crit_HE(r) = (crit_tau_HE(r) || crit_nan_HE(r));
         
-        crit_tau_PATV_HE(r) = abs(tau_error_PATV_HE(r,1))>2;
-        crit_nan_PATV_HE(r) = isnan(tau_error_PATV_HE(r,1));
-        crit_PATV_HE(r) = (crit_tau_PATV_HE(r) || crit_nan_PATV_HE(r));
+%         crit_tau_PATV_HE(r) = abs(tau_error_PATV_HE(r,1))>2;
+%         crit_nan_PATV_HE(r) = isnan(tau_error_PATV_HE(r,1));
+%         crit_PATV_HE(r) = (crit_tau_PATV_HE(r) || crit_nan_PATV_HE(r));
         
     %        eps_NLHE = eps_NLHE + (tau_error_NLHE(r,1)>2 || isnan(tau_error_NLHE(r,1)));
         end
         false_pos_HE(ps,s) = sum(crit_tau_HE)*100/MCruns;
         false_neg_HE(ps,s) = sum(crit_nan_HE)*100/MCruns;
-        false_pos_PATV_HE(ps,s) = sum(crit_tau_PATV_HE)*100/MCruns;
-        false_neg_PATV_HE(ps,s) = sum(crit_nan_PATV_HE)*100/MCruns;
+%         false_pos_PATV_HE(ps,s) = sum(crit_tau_PATV_HE)*100/MCruns;
+%         false_neg_PATV_HE(ps,s) = sum(crit_nan_PATV_HE)*100/MCruns;
         
         eps_HE(ps,s) = sum(crit_HE)*100/MCruns; % in [%];
-        eps_PATV_HE(ps,s) = sum(crit_PATV_HE)*100/MCruns; % in [%];
+%        eps_PATV_HE(ps,s) = sum(crit_PATV_HE)*100/MCruns; % in [%];
         
         FE_std_HE(ps,s) = std(FE_HE)/f0;
         FE_mean_HE(ps,s) = mean(FE_HE)/f0;
         FE_box(s,:) = FE_HE/f0; %[Hz/Hz]
 
-        FE_std_PATV_HE(ps,s) = std(FE_PATV_HE)/f0;
-        FE_mean_PATV_HE(ps,s) = mean(FE_PATV_HE)/f0;
-        FE_box(s,:) = FE_PATV_HE/f0; %[Hz/Hz]
+%         FE_std_PATV_HE(ps,s) = std(FE_PATV_HE)/f0;
+%         FE_mean_PATV_HE(ps,s) = mean(FE_PATV_HE)/f0;
+%         FE_box(s,:) = FE_PATV_HE/f0; %[Hz/Hz]
         
     end
 end
@@ -69,11 +69,11 @@ end
 %FE_std_HE
 %FE_std_PATV_HE
 eps_HE
-eps_PATV_HE
+%eps_PATV_HE
 beep
-FE_corr = FE_mean_PATV_HE - 0.002213
-dmax_mean = mean(dmax)
-dmax_std = std(dmax)
+%FE_corr = FE_mean_PATV_HE - 0.002213
+% dmax_mean = mean(dmax)
+% dmax_std = std(dmax)
 %createfigure1(SNR, HE_eps')
 %createfigure2(SNR, HE_eps')
 
