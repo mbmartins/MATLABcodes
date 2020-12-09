9482a2ceadfbcc0ee85988e8084e188abf93d751
mbmartins <mbrittom@gmail.com>
2020-12-09 16:51:26 -0300

    completei agora os graficos ALL

--- ROCOF/salto freq/graficos_f1.m
+++ ROCOF/salto freq/graficos_f1.m
@@ -37,7 +37,7 @@ shade(F1_vec,FE1(:,k) - f1_std(:,k),[c(k),'-.'],F1_vec,FE1(:,k) + f1_std(:,k),[c
 %plot(tau_n,FE(:,k) - FE_std(:,k),[c(k),'--']); 
 grid on;
 %xlim([48,432]); 
-ylim([-1 1.])
+ylim([-0.3 0.6])
 % legend(EF, 'EF1','EF2','EF3','EF4','EF5','EF6')
 % legend('Orientation','horizontal')
 % legend('Location','northoutside')
@@ -75,3 +75,41 @@ end
 subplot(3,2,4); ylim([-1.25 -0.75])
 savefig(fig3,'salto_freq_KFE_F1.fig')
 saveas(fig3,'salto_freq_KFE_F1.png')
+% 
+% fig4 = figure('Units','normalized','Position',[0 0 0.5 0.5]);
+% ------------- ALL com ruído de 60 dB -------------
+for k = 1:6
+    figALL(k) = figure('Units','normalized','Position',[0 0 0.5 0.13]);
+    subplot(1,3,1)
+    pleg(1) = plot(F1_vec,FE(:,k),c(k)+"-"); hold on;
+    shade(F1_vec,FE(:,k) - FE_std(:,k),[c(k),'-.'],F1_vec,FE(:,k) + FE_std(:,k),[c(k),'--'], 'FillType', [1,2; 2,1]);
+    xlabel('f_1 [Hz]'); ylabel('FE [Hz]')
+    ylim([-0.3 0.1])
+grid on;
+    subplot(1,3,2)
+    pleg(2) = plot(F1_vec,KFE(:,k),c(k)+"-"); hold on;
+    shade(F1_vec,KFE(:,k) - kf_std(:,k),[c(k),'--'],F1_vec,KFE(:,k) + kf_std(:,k),[c(k),'--'], 'FillType', [1,2; 2,1]);
+    xlabel('f_1 [Hz]'); ylabel('K_fE [Hz]')
+    ylim([-1.1 0.1])
+    grid on;
+    subplot(1,3,3)
+    pleg(3) = plot(F1_vec,FE1(:,k),c(k)+"-"); hold on;
+    shade(F1_vec,FE1(:,k) - f1_std(:,k),[c(k),'--'],F1_vec,FE1(:,k) + f1_std(:,k),[c(k),'--'], 'FillType', [1,2; 2,1]);
+    xlabel('f_1 [Hz]'); ylabel('FE_1 [Hz]')
+    grid on;
+    ylim([-0.3 0.6])
+    %legend(pleg,'FE');
+%plot(tau_n,FE(:,k) - FE_std(:,k),[c(k),'--']); 
+
+%xlim([48,432]); 
+%ylim([-0.4 0.1])
+% legend(EF, 'EF1','EF2','EF3','EF4','EF5','EF6')
+% legend('Orientation','horizontal')
+% legend('Location','northoutside')
+%legend('boxoff')
+%title('FE com ruido SNR = 60 dB')
+%title(tits(k));
+savefig(figALL(k),"salto_freq_ALL_F1"+"_EF"+k)
+saveas(figALL(k),"salto_freq_ALL_F1"+"_EF"+k+".png")
+end
+% --------------------------------------------------
